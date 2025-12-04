5f607061539b696de7d883c5844899662426f55b
Ikhbar <114275801+N3XX3R71@users.noreply.github.com>
2025-12-04 19:10:27 +0700

    Add files via upload

---
+++ JurnalBeritaSI/Config/constants.php
@@ -0,0 +1,7 @@
+<?php 
+session_start();
+define('ROOT_URL', 'http://localhost/jurnalberitasi/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'Ikhbar');
+define('DB_PASS', 'admin1234');
+define('DB_NAME', 'berita_si');
